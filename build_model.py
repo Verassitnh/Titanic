4dccb22b73b23b136ccf8d1881b7651736b9146e
Jackson Mooring <jackson.mooring@gmail.com>
2021-07-08 12:00:38 -0500

    Add structure

--- build_model.py
+++ build_model.py
@@ -5,7 +5,7 @@ import joblib
 
 
 
-df = pd.read_csv('https://cdn.techroulette.xyz/projects/shipwreck/data/m3_data.csv')
+df = pd.read_csv('data.cvs')
 
 df = df.drop(["PassengerId", "Name", "Ticket", "Cabin", "Fare", "Embarked"], axis=1)
 df = df.replace({'male': 0, 'female': 1})
@@ -15,9 +15,7 @@ df = df.dropna()
 Y = df.pop('Survived')
 X = df
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.15)
-
 model = DecisionTreeClassifier()
-model.fit(X_train, Y_train)
+model.fit(X, Y)
 
 joblib.dump(model, "model.ml")
