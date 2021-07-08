389e9c38b5046db3804ded95013e46eb112c8aa3
Jackson Mooring <jackson.mooring@gmail.com>
2021-07-08 12:38:03 -0500

    Add Server

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+from flask import Flask
 import joblib
 
+app = Flask(__name__)
 model = joblib.load("model.ml")
 
 def predict(pclass ,age, sex, sibSp, parCh):
     return model.predict([[pclass, sex, age, sibSp, parCh]])
+
+
+
+
+@app.route("/api/predict/<int:pclass>/<int:age>/<int:sex>/<int:sibsp>/<int:parch>")
+def hello_world(pclass, age, sex, sibsp, parch):
+    return "You will die" if predict(pclass, age, sex, sibsp, parch)[0] == 0 else "You will live"
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host="0.0.0.0", port=80)
